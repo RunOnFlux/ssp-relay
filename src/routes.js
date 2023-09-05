b40e9c5d2e250137f5d162a38ccf546754ca8adc
TheTrunk <tadeas.kmenta@zel.cash>
2023-09-05 16:15:58 +0200

    add token endpoints

--- src/routes.js
+++ src/routes.js
@@ -10,9 +10,15 @@ module.exports = (app) => {
   app.get('/v1/action/:id?', (req, res) => {
     actionApi.getAction(req, res);
   });
+  app.get('/v1/token/:id?', (req, res) => {
+    syncApi.getToken(req, res);
+  });
   // post sync data
   app.post('/v1/sync', (req, res) => {
     syncApi.postSync(req, res);
+  });
+  app.post('/v1/token', (req, res) => {
+    syncApi.postToken(req, res);
   });
   // post sync data
   app.post('/v1/action', (req, res) => {
