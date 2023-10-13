d3c1bf0f9d9d3e951fe29c48e7c4da47d6668f96
TheTrunk <tadeas.kmenta@zel.cash>
2023-10-13 21:59:48 +0200

    fix tx decoding

--- src/services/transactionDecoder.js
+++ src/services/transactionDecoder.js
@@ -13,17 +13,29 @@ function decodeTransactionForApproval(
       network,
     );
     console.log(JSON.stringify(txb));
-    let txReceiver = '';
+    let txReceiver = 'decodingError';
     let amount = '0';
     let senderAddress = '';
-    const scriptPubKey = utxolib.script.scriptHash.output.encode(
-      utxolib.crypto.hash160(txb.inputs[0].redeemScript),
-    );
 
-    senderAddress = utxolib.address.fromOutputScript(
-      scriptPubKey,
-      network,
-    );
+    if (txb.inputs[0].witnessScript && txb.inputs[0].redeemScript) {
+      // p2sh-p2wsh
+      const scriptPubKey = utxolib.script.scriptHash.output.encode(
+        utxolib.crypto.hash160(txb.inputs[0].redeemScript),
+      );
+      senderAddress = utxolib.address.fromOutputScript(scriptPubKey, network);
+    } else if (txb.inputs[0].witnessScript) {
+      // p2wsh
+      const scriptPubKey = utxolib.script.witnessScriptHash.output.encode(
+        utxolib.crypto.sha256(txb.inputs[0].witnessScript),
+      );
+      senderAddress = utxolib.address.fromOutputScript(scriptPubKey, network);
+    } else {
+      // p2sh
+      const scriptPubKey = utxolib.script.scriptHash.output.encode(
+        utxolib.crypto.hash160(txb.inputs[0].redeemScript),
+      );
+      senderAddress = utxolib.address.fromOutputScript(scriptPubKey, network);
+    }
     txb.tx.outs.forEach((out) => {
       if (out.value) {
         const address = utxolib.address.fromOutputScript(out.script, network);
