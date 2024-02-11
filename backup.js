917779298863bff26457abe57c2d801138720981
TheTrunk <tadeas.kmenta@zel.cash>
2024-02-11 22:35:15 +0700

    name

--- backup.js
+++ backup.js
@@ -13,11 +13,11 @@ async function makeBackup() {
     const date = new Date().getTime();
     await cmdAsync('rm -rf dumpOld.tar.gz');
     await cmdAsync(`mongodump --db ${config.database.database}`);
-    await cmdAsync(`tar -czvf fluxStorageMongoDump${date}.tar.gz dump`);
+    await cmdAsync(`tar -czvf sspMongoDump${date}.tar.gz dump`);
     await cmdAsync('rm -rf dump');
-    await cmdAsync(`rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress fluxStorageMongoDump${date}.tar.gz ${ip}:/root/fluxStorageBackupDumps/`);
-    await cmdAsync(`rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress fluxStorageMongoDump${date}.tar.gz ${ipB}:/root/fluxStorageBackupDumps/`);
-    await cmdAsync(`mv fluxStorageMongoDump${date}.tar.gz dumpOld.tar.gz`);
+    await cmdAsync(`rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress sspMongoDump${date}.tar.gz ${ip}:/root/sspBackupDumps/`);
+    await cmdAsync(`rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress sspMongoDump${date}.tar.gz ${ipB}:/root/sspBackupDumps/`);
+    await cmdAsync(`mv sspMongoDump${date}.tar.gz dumpOld.tar.gz`);
   } catch (error) {
     console.log(error);
   }
