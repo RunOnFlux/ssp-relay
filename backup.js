6b6cf4b5e0aefea20296c634360b3e19d7ec1ae1
TheTrunk <tadeas.kmenta@zel.cash>
2024-02-11 22:33:59 +0700

    backup file bump pacakges

---
+++ backup.js
@@ -0,0 +1,29 @@
+const cmd = require('node-cmd');
+const config = require('config');
+const util = require('util');
+
+const ip = '';
+const ipB = '';
+
+const cmdAsync = util.promisify(cmd.run);
+// create dump of db
+
+async function makeBackup() {
+  try {
+    const date = new Date().getTime();
+    await cmdAsync('rm -rf dumpOld.tar.gz');
+    await cmdAsync(`mongodump --db ${config.database.database}`);
+    await cmdAsync(`tar -czvf fluxStorageMongoDump${date}.tar.gz dump`);
+    await cmdAsync('rm -rf dump');
+    await cmdAsync(`rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress fluxStorageMongoDump${date}.tar.gz ${ip}:/root/fluxStorageBackupDumps/`);
+    await cmdAsync(`rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress fluxStorageMongoDump${date}.tar.gz ${ipB}:/root/fluxStorageBackupDumps/`);
+    await cmdAsync(`mv fluxStorageMongoDump${date}.tar.gz dumpOld.tar.gz`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+makeBackup();
+setInterval(async () => {
+  makeBackup();
+}, 60 * 60 * 1000);
