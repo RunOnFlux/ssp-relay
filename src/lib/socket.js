2068f060bf17c688bf2ddf2d52af81dc57fe5501
TheTrunk <tadeas.kmenta@zel.cash>
2024-07-02 21:16:12 +0800

    minor

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -11,7 +11,7 @@ function initIOKey(httpServer, path = '/v1/socket/key') {
     socket.on('join', async ({ wkIdentity }) => {
       socket.join(wkIdentity);
       const actionToSend = await socketService.getAction(wkIdentity).catch();
-      if (actionToSend.action === 'tx') {
+      if (actionToSend.action === 'tx' || actionToSend.action === 'publicnoncesrequest') {
         ioKey.to(wkIdentity).emit(actionToSend.action, actionToSend);
       }
     });
