15028e00ef4ba4312ceb5308f005ade0daee3ed8
TheTrunk <tadeas.kmenta@zel.cash>
2025-01-26 20:26:46 +0700

    update dependencies add rate limiting

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -2,18 +2,29 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import compression from 'compression';
+import { rateLimit } from 'express-rate-limit';
 import routes from '../routes';
 
 const nodeEnv = process.env.NODE_ENV;
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 if (nodeEnv !== 'test') {
   app.use(morgan('combined'));
 }
 
+const limiter = rateLimit({
+  windowMs: 30 * 1000, // 30 seconds
+  max: 120, // Limit each IP to 120 requests per windowMs
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+});
+
 app.use(compression());
 app.use(cors());
+app.use(limiter);
 routes(app);
 
 export default app;
