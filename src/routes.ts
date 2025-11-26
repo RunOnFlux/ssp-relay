c7b4b5487576d5841ed2cf303eb73b997937bcf8
TheTrunk <tadeas@runonflux.io>
2025-11-26 19:28:25 +0800

    fix express 5

--- src/routes.ts
+++ src/routes.ts
@@ -10,10 +10,10 @@ import onramperApi from './apiServices/onramperApi';
 
 export default (app) => {
   // return sync data
-  app.get('/v1/sync/:id?', (req, res) => {
+  app.get('/v1/sync{/:id}', (req, res) => {
     syncApi.getSync(req, res);
   });
-  app.get('/v1/action/:id?', (req, res) => {
+  app.get('/v1/action{/:id}', (req, res) => {
     actionApi.getAction(req, res);
   });
   // post sync data
@@ -44,7 +44,7 @@ export default (app) => {
     contactApi.postContact(req, res);
   });
   // get token information endpoint
-  app.get('/v1/tokeninfo/:network?/:address?', (req, res) => {
+  app.get('/v1/tokeninfo{/:network}{/:address}', (req, res) => {
     tokenApi.getTokenInfo(req, res);
   });
   // get enabled services, used to toggle off on third party features in the app
